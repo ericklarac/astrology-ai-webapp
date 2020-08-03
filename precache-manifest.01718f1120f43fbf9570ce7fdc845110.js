2603988ff3d4a50879045e488c2bd1e32d7af70a
Erick Lara <ericklarita@gmail.com>
2020-08-03 11:07:01 -0500

    Updates

---
+++ precache-manifest.01718f1120f43fbf9570ce7fdc845110.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01a54bf5babc13941fe1d9165e04704b",
+    "url": "/astrology-ai-webapp/index.html"
+  },
+  {
+    "revision": "f6c66aab44c09e2b3f6d",
+    "url": "/astrology-ai-webapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1a45e7c7674a571ba53d",
+    "url": "/astrology-ai-webapp/static/js/2.63cc4000.chunk.js"
+  },
+  {
+    "revision": "4c98c6b556b8cc39dae85626e0284631",
+    "url": "/astrology-ai-webapp/static/js/2.63cc4000.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6c66aab44c09e2b3f6d",
+    "url": "/astrology-ai-webapp/static/js/main.4c199ac5.chunk.js"
+  },
+  {
+    "revision": "eda1fb28f16b5e13a9cc",
+    "url": "/astrology-ai-webapp/static/js/runtime-main.476486bf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/astrology-ai-webapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
